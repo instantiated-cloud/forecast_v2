fd560bf5b03d4b44c43a2a72ef0989279509201d
instantiated-cloud <mher6016@gmail.com>
2026-03-16 05:14:49 +0000

    Removed comments from load_data

--- th_kh_conflict_forecast/src/load_data.py
+++ th_kh_conflict_forecast/src/load_data.py
@@ -1,25 +1,3 @@
-# import pandas as pd
-# from .config import DATA_RAW
-
-# def load_incidents():
-#     return pd.read_csv(f"{DATA_RAW}/incidents.csv", parse_dates=["date"])
-
-# def load_military():
-#     return pd.read_csv(f"{DATA_RAW}/military_activity.csv", parse_dates=["date"])
-
-# def load_news():
-#     return pd.read_csv(f"{DATA_RAW}/news_sentiment.csv", parse_dates=["date"])
-
-# def load_trade():
-#     return pd.read_csv(f"{DATA_RAW}/trade_migration.csv", parse_dates=["date"])
-
-# def load_displacement():
-#     return pd.read_csv(f"{DATA_RAW}/displacement.csv", parse_dates=["date"])
-
-# def load_segments():
-#     return pd.read_csv(f"{DATA_RAW}/segments.csv")
-
-
 import pandas as pd
 from .config import DATA_RAW
 
